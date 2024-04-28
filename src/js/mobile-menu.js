3040e836ad8d6ebb774e76928956ba69e1b2d3e5
OleksijHr <hrymovskij@gmail.com>
2024-04-28 22:14:27 +0300

    comit

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,12 +1,14 @@
-const refs = {
-  openMenuBtn: document.querySelector('[data-menu-open]'),
-  closeMenuBtn: document.querySelector('[data-menu-close]'),
-  menu: document.querySelector('[data-menu]'),
-};
+// логіка секції
 
-refs.openMenuBtn.addEventListener('click', toggleModal);
-refs.closeMenuBtn.addEventListener('click', toggleModal);
+// const refs = {
+//   openMenuBtn: document.querySelector('[data-menu-open]'),
+//   closeMenuBtn: document.querySelector('[data-menu-close]'),
+//   menu: document.querySelector('[data-menu]'),
+// };
 
-function toggleModal() {
-  refs.menu.classList.toggle('is-open');
-}
+// refs.openMenuBtn.addEventListener('click', toggleModal);
+// refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+// function toggleModal() {
+//   refs.menu.classList.toggle('is-open');
+// }
