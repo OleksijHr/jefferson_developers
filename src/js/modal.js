a0652e76b616f5b6d7e172db7aa3653c0c034eb8
pavlo <pashapasha600@gmail.com>
2024-04-30 09:37:10 +0200

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,30 @@
-// логіка секції
+ document.addEventListener('DOMContentLoaded', function () {
+    // Отримуємо посилання на модальне вікно та кнопку, яка його відкриває
+    const modal = document.getElementById('myModal');
+    const openModalBtn = document.getElementById('openModalBtn');
+
+    // Відкриття модального вікна при кліку на кнопку
+    openModalBtn.addEventListener('click', function () {
+      modal.style.display = 'block';
+    });
+
+    // Закриття модального вікна по кліку на кнопку закриття
+    const closeModalBtn = modal.querySelector('.close');
+    closeModalBtn.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+
+    // Закриття модального вікна по кліку на backdrop
+    window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+
+    // Закриття модального вікна по натисканню на клавішу Escape
+    window.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        modal.style.display = 'none';
+      }
+    });
+  });
