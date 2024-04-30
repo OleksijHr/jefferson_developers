b9738da87199cde1eb8c81a688875e71b1ccb1d6
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2024-04-30 18:39:12 +0100

    upd-final-pull

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,23 +3,4 @@ import 'accordion-js/dist/accordion.min.css';
 
 const container = document.querySelector('.accordion-container');
 
-new Accordion(container, {
-  onOpen: function (currentElement) {
-    const faqBtn = document.querySelector('.ac-trigger');
-
-    console.log(currentElement);
-  },
-});
-
-// faqs.forEach(faq => {
-//   const accordion = new Accordion(faq, {
-//     duration: 400,
-//     showMultiple: true,
-
-//
-//   });
-
-//   faq.addEventListener('click', () => {
-//     faq.classList.toggle('is-active');
-//   });
-// });
+new Accordion(container, {});
