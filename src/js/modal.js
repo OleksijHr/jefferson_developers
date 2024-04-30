c58f507f1597ea3573a8c2c8086d3f7725513bd0
OleksijHr <hrymovskij@gmail.com>
2024-04-30 15:52:50 +0300

    corection modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,8 @@
+const body = document.body;
+const modal = document.getElementById('myModal');
+const form = document.querySelector('.footer_form'); 
+
 document.addEventListener('DOMContentLoaded', function () {
-    const modal = document.getElementById('myModal');
-    const form = document.querySelector('.footer_form'); 
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -9,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
        
         modal.style.display = 'block';
+        hideScroll();
         form.reset();
     });
 
@@ -16,12 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeModalBtn = modal.querySelector('.close');
     closeModalBtn.addEventListener('click', function () {
         modal.style.display = 'none';
+        openScroll()
     });
 
     
     window.addEventListener('click', function (event) {
         if (event.target === modal) {
             modal.style.display = 'none';
+            openScroll()
         }
     });
 
@@ -29,6 +34,26 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('keydown', function (event) {
         if (event.key === 'Escape') {
             modal.style.display = 'none';
+            openScroll()
         }
     });
 });
+
+function hideScroll() {
+    body.style.height = '100vh';
+    body.style.overflowY = 'hidden';
+    body.style.paddingRight = '15px';
+
+    modal.style.height = '100vh';
+    modal.style.overflowY = 'hidden';
+
+}
+
+function openScroll() {
+    body.style.height = 'auto';
+    body.style.overflowY = 'auto';
+    body.style.paddingRight = 'none';
+
+    modal.style.height = 'auto';
+    modal.style.overflowY = 'auto';
+}
