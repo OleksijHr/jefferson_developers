36b945c7cb3476241ab9dbb009069e336ce3314c
OMatv <149325870+OMatv@users.noreply.github.com>
2024-05-01 18:29:53 +0300

    css

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,56 +1,68 @@
-// імпортувати з api.js
-// логіка секції
-
-import * as api from './api';
-import './swiper';
-
-// import Swiper JS
 import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import * as api from './api';
 
-//             обгортка повинна включати усі елементи для свайперу
-//                                    |
-// export const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-//   slidesPerView: 2,
-//   slidesPerGroup: 1,
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+document.addEventListener('DOMContentLoaded', async () => {
+  const reviewsList = document.querySelector('.menu-reviews-list-ul');
 
-const reviewsList = document.querySelector('.menu-reviews-list-ul');
+  try {
+    const data = await api.searchReviews();
 
-api
-  .searchReviews()
-  .then(data => {
-    console.log(data);
     reviewsList.insertAdjacentHTML('beforeend', createReviews(data));
-  })
-  .catch(error => {
-    console.log(error);
-  });
+
+    const swiper = new Swiper('.swiper-reviews-wrap', {
+      direction: 'horizontal',
+      modules: [Navigation],
+      slidesPerView: 1,
+      spaceBetween: 16,
+      breakpoints: {
+        // when window width is >= 375px
+        375: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 1440px
+        1440: {
+          slidesPerView: 4,
+        },
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+  }
+});
 
 function createReviews(arrReviews) {
   return arrReviews
     .map(({ _id, author, avatar_url, review }) => {
-      return `<li class="menu-reviews-item-li menu-item swiper-slide-reviews" id="${_id}">
-         <img
-           class="icon-reviews-img"
-           src="${avatar_url}"
-           width="48"
-           height="48"
-           alt="${author}'s avatar"
-         />
-         <div class="container-reviews-title-text">
-           <h3 class="reviews-title-h3">${author}</h3>
-           <p class="reviews-text">${review}</p>
-         </div>
-      </li>`;
+      return `
+        <li class="menu-reviews-item-li menu-item swiper-slide" id="${_id}">
+          <img
+            class="icon-reviews-img"
+            src="${avatar_url}"
+            width="48"
+            height="48"
+            alt="${author}'s avatar"
+          />
+          <div class="container-reviews-title-text">
+            <h3 class="reviews-title-h3">${author}</h3>
+            <p class="reviews-text">${review}</p>
+          </div>
+        </li>
+      `;
     })
     .join('');
 }
