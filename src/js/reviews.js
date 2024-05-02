20f5eac4b22b21d3ee771f309b071a63161cb79f
OleksijHr <hrymovskij@gmail.com>
2024-05-02 23:24:02 +0300

    correction reviews swiper and section's css

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -23,10 +23,41 @@ api.searchReviews()
       modules: [Navigation],
       slidesPerView: 2,
       spaceBetween: 30,
+      speed: 400,
+      loop: true,
+      setWrapperSize: true,
 
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+      },
+    breakpoints: {
+      // Налаштування для десктопу
+      1440: {
+        slidesPerView: 4,
+      },
+      // Налаштування для планшету
+      768: {
+        slidesPerView: 2,
+      },
+      // Налаштування для мобільних пристроїв
+      320: {
+        slidesPerView: 1,
+      },
+    },
+    
+    a11y: {
+  prevSlideMessage: 'Previous slide',
+  nextSlideMessage: 'Next slide',
+      },
+    
+    keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+      },
+    
+    mousewheel: {
+    invert: true,
       },
     });
   })
@@ -38,6 +69,7 @@ function createReviews(arrReviews) {
   return arrReviews 
     .map(({ _id, author, avatar_url, review }) => { 
       return `<li class="menu-reviews-item-li menu-item swiper-slide" id="${_id}">  
+         <div class="reviews-img-box">
          <img 
            class="icon-reviews-img" 
            src="${avatar_url}" 
@@ -45,6 +77,7 @@ function createReviews(arrReviews) {
            height="48" 
            alt="${author}'s avatar" 
          /> 
+         </div>
          <div class="container-reviews-title-text"> 
            <h3 class="reviews-title-h3">${author}</h3> 
            <p class="reviews-text">${review}</p> 
